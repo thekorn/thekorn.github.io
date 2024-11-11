742d309b1a3ccc2d63075cd68b8fe19ee311be20
Markus Korn <markus.korn@gmail.com>
2024-11-11 17:40:39 +0100

    Refactor UI and streamline project structure
    - Refactor and simplify UI components across the application, removing unnecessary elements and files such as `Counter.tsx` and `about.tsx`.
    - Overhaul global styles with the introduction of new CSS rules in `app.css`, implementing responsive designs and a grid system.
    - Streamline navigation by updating the Nav component and home page links, removing non-essential links, and enhancing usability and aesthetics.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,13 +7,10 @@ export default function Nav() {
       ? "border-sky-600"
       : "border-transparent hover:border-sky-600";
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
+    <nav class="">
+      <ul class="container flex items-center p-3">
         <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
           <a href="/">Home</a>
-        </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
         </li>
       </ul>
     </nav>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,44 +1,49 @@
 import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
+import { For } from "solid-js";
 
 export default function Home() {
+  const links = [
+    "https://github.com/thekorn",
+    "https://www.linkedin.com/in/markuskorn/",
+  ];
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        <div class="flex justify-center items-center">
-          <span class="mr-5">Hello</span> <img class="w-12 h-12" src="https://unocss.dev/logo.svg" alt="UnoCSS logo" />!
+    <main class="mt-1 flex-auto">
+      <h1 class="max-6 text-6xl font-bold uppercase my-16">thekorn.dev</h1>
+      <section class="flex flex-col sm:flex-row gap-6 sm:gap-3">
+        <div class="font-medium">
+          <p class="mb-1">
+            Welcome to my personal website. I'm a software engineer and I love
+            building things for the web.
+          </p>
+          <p>
+            Connect with me on{" "}
+            <a
+              class="underline"
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:contact@thekorn.dev"
+            >
+              contact@thekorn.dev
+            </a>
+          </p>
         </div>
-      </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="mt-2">
-        Visit{" "}
-        <a
-          href="https://unocss.dev"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          unocss.dev
-        </a>{" "}
-        to learn how to style your app.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+        <ul class="sm:mt-3 text-slate-600 dark:text-slate-200 text-base sm:text-sm leading-1">
+          <For each={links}>
+            {(link) => (
+              <li class="list-square hover:text-black dark:hover:text-white ml-2 ">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={link}
+                  class="underline"
+                >
+                  {link}
+                </a>
+              </li>
+            )}
+          </For>
+        </ul>
+      </section>
     </main>
   );
 }

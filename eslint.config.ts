ce4946aa05691cd377c4003b919e688ef1365ad0
Markus Korn <markus.korn@gmail.com>
2025-12-25 13:04:12 +0100

    Integrate ESLint for enhanced code quality
    - Replace Biome with ESLint for code linting to improve consistency and customization.
    - Introduce a comprehensive ESLint configuration supporting multiple languages and file types.
    - Update `package.json` dependencies to include necessary ESLint packages.

---
+++ eslint.config.ts
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default defineConfig([
+  globalIgnores([
+		"dist/", ".astro"
+	]),
+  ...eslintPluginAstro.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  //tseslint.configs.recommended,
+  //{ files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  //{ files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);

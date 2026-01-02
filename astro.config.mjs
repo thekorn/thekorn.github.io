9ca7aca39b9e90a8978cfbc533e04e3b0b9af405
Markus Korn <markus.korn@gmail.com>
2026-01-02 22:44:49 +0100

    Integrate MDX support and add Pokeball blog draft
    - Integrate MDX support into the Astro project to enhance content management capabilities.
    - Add a blog post draft on creating a Pokeball with Tailwind CSS, expanding the project's tutorial section.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.thekorn.dev",
-  integrations: [solidJs(), sitemap()],
+  integrations: [solidJs(), sitemap(), mdx()],
 
   vite: {
     plugins: [tailwindcss()],

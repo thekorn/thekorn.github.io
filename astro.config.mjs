63f2bab2ccdf9343377b3cccfee2997c965c69e4
Markus Korn <markus.korn@gmail.com>
2026-01-03 07:21:56 +0100

    Enhance URL parsing with optional chaining
    - Improve URL parsing robustness in sitemap by adding optional chaining.
    - Enhance error handling in the sitemap filter function for better fault tolerance.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     solidJs(),
     sitemap({
-      filter: (page) => !URL.parse(page).pathname.startsWith("/challenges/"),
+      filter: (page) => !URL.parse(page)?.pathname.startsWith("/challenges/"),
     }),
     mdx(),
   ],

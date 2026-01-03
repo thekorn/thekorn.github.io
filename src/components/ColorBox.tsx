118eb290e4f9130cc11a867b7f7dfa2fdcc0cde3
Markus Korn <markus.korn@gmail.com>
2026-01-03 12:25:02 +0100

    Improve accessibility and styling consistency
    - Update `ColorBox.tsx` to ensure consistent aspect ratio using `aspect-square`.
    - Enhance accessibility by adding a title to the Pokeball Challenge iframe.

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,7 +1,7 @@
 const ColorBox = (props: { color: string }) => {
   return (
     <span
-      class="before:w-4 before:h-4  before:bg-(--color) before:mr-2 before:inline-block before:align-middle before:border before:border-(--tw-prose-body)"
+      class="before:h-4 before:aspect-square before:bg-(--color) before:mr-2 before:inline-block before:align-middle before:border before:border-(--tw-prose-body)"
       style={`--color:${props.color}`}
     >
       {props.color}

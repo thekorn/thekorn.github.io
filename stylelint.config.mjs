6d79456eb379072b3b9a8ad37c9a790f62adddcf
Markus Korn <markus.korn@gmail.com>
2026-01-03 13:26:40 +0100

    Integrate Stylelint for consistent CSS validation
    - Implement CSS formatting and validation using Stylelint with enhanced settings for Tailwind CSS.
    - Integrate Stylelint into the development workflow, including as part of the test suite.
    - Update project dependencies to include necessary Stylelint packages and configurations.

---
+++ stylelint.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import("stylelint").Config} */
+export default {
+  ignoreFiles: ['node_modules/**/*', 'dist/**/*'],
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+};
